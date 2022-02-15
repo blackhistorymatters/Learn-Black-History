37e5a7f15857d20e1b08f10e894a7f76d1b7405b
AoifeWM <aoifewmulligan@gmail.com>
2022-02-14 20:19:21 -0800

    Added fucntionality for the facts page

--- src/FactsPage.js
+++ src/FactsPage.js
@@ -8,194 +8,191 @@ import Button from 'react-bootstrap/Button';
 
 class FactsPage extends React.Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: null,
-      tagList: [],
-      peopleList: [],
-      selectedTag: '',
-      selectedPerson: '',
-      factByTag: [],
-      factByPerson: [],
-      userFavArr: []
-    }
-  }
-
-  async componentDidMount() {
-    const config = {
-      method: 'get',
-      baseURL: process.env.REACT_APP_SERVER,
-      url: '/tagslist'
+    constructor(props) {
+        super(props);
+        this.state = {
+            tagList: [],
+            peopleList: [],
+            selectedTag: '',
+            selectedPerson: '',
+            displayedFacts: [],
+        }
     }
 
-    const peopleConfig = {
-      method: 'get',
-      baseURL: process.env.REACT_APP_SERVER,
-      url: '/peoplelist'
+    async componentDidMount() {
+        const config = {
+            method: 'get',
+            baseURL: process.env.REACT_APP_SERVER,
+            url: '/tagslist'
+        }
+
+        const peopleConfig = {
+            method: 'get',
+            baseURL: process.env.REACT_APP_SERVER,
+            url: '/peoplelist'
+        }
+
+        const tagsResponse = await axios(config);
+
+
+        this.setState({ tagList: tagsResponse.data });
+        console.log('The list of tags is here', this.state.tagList);
+
+        const peopleResponse = await axios(peopleConfig);
+
+
+        this.setState({ peopleList: peopleResponse.data });
+        console.log('The list of people is here', this.state.peopleList);
     }
 
-    const tagsResponse = await axios(config);
-  
+    handleTagChange = async (event) => {
+        event.preventDefault();
+        const tagSelection = event.target.value;
+        await this.setState({ selectedTag: tagSelection });
+        console.log(this.state.selectedTag)
 
-    this.setState({ tagList: tagsResponse.data });
-    console.log('The list of tags is here', this.state.tagList);
+        this.getFactByTags();
 
-    const peopleResponse = await axios(peopleConfig);
-    
+    }
+    handlePeopleChange = async (event) => {
+        event.preventDefault();
+        const peopleSelection = event.target.value;
+        await this.setState({ selectedPerson: peopleSelection });
+        console.log(this.state.selectedPerson);
 
-    this.setState({ peopleList: peopleResponse.data });
-    console.log('The list of people is here', this.state.peopleList);
-  }
-
-  handleTagChange = async (event) => {
-    event.preventDefault();
-    const tagSelection = event.target.value;
-    await this.setState({ selectedTag: tagSelection });
-    console.log(this.state.selectedTag)
-
-    this.getFactByTags();
-
-  }
-  handlePeopleChange = async (event) => {
-    event.preventDefault();
-    const peopleSelection = event.target.value;
-    await this.setState({ selectedPerson: peopleSelection });
-    console.log(this.state.selectedPerson);
-
-    this.getFactsByPerson();
-  }
-
-  async getFactByTags() {
-
-    const queryObj = this.state.selectedTag;
-    console.log(queryObj);
-    const config = {
-      params: { tags: queryObj },
-      method: 'get',
-      baseURL: process.env.REACT_APP_SERVER,
-      url: '/fact'
+        this.getFactsByPerson();
     }
 
-    const factTagResponse = await axios(config)
-    this.setState({ factByTag: factTagResponse.data.Results })
-    console.log(factTagResponse.data);
-  }
+    async getFactByTags() {
 
-  async getFactsByPerson() {
+        const queryObj = this.state.selectedTag;
+        console.log(queryObj);
+        const config = {
+            params: { tags: queryObj },
+            method: 'get',
+            baseURL: process.env.REACT_APP_SERVER,
+            url: '/fact'
+        }
 
-    const queryObj = this.state.selectedPerson;
-    console.log(queryObj);
-    const config = {
-      params: { people: queryObj },
-      method: 'get',
-      baseURL: process.env.REACT_APP_SERVER,
-      url: '/people'
+        const factTagResponse = await axios(config)
+        this.setState({ displayedFacts: factTagResponse.data.Results })
+        console.log(factTagResponse.data);
     }
-    console.log(this.state.factByPerson)
-    const factPersonResponse = await axios(config)
-    this.setState({ factByPerson: factPersonResponse.data.Results })
-    console.log('This is a person', this.state.factByPerson);
-  }
 
-  // createFact = async (factPeople, factText, factSource) => {
-  //   if (this.props.auth0.isAuthenticated) {
-  //     const res = await this.props.auth0.getIdTokenClaims();
-  //     const jwt = res.__raw;
-  //     console.log("jwt: ", jwt);
-  //     let newFact = {
-  //       people: factPeople,
-  //       text: factText,
-  //       source: factSource,
-  //     }
-  //     console.log('New fact being created (location factspage.js) FROM FORM unless otherwise specified', newFact);
-  //     const config = {
-  //       headers: { "Authorization": `Bearer ${jwt}` },
-  //       params: { email: this.props.auth0.user.email },
-  //       method: 'post',
-  //       baseURL: process.env.REACT_APP_SERVER,
-  //       url: `/userfacts`,
-  //       data: newFact
-  //     }
-  //     const createResponse = await axios(config);
-  //     console.log(createResponse.data);
-  //     let newFavArr = this.state.userFavArr;
-  //     newFavArr.push(newFact);
-  //     this.setState({ userFavArr: newFavArr })
-  //   } else {
-  //     console.error("Invalid authentification.")
-  //   }
-  // }
+    async getFactsByPerson() {
 
-  // personFactFavorite = async (fact) => {
-  //   console.log('New fact being created by FAVORITING', fact);
-  //   await this.createFact(fact.people, fact.text, fact.source);
-  // }
+        const queryObj = this.state.selectedPerson;
+        console.log(queryObj);
+        const config = {
+            params: { people: queryObj },
+            method: 'get',
+            baseURL: process.env.REACT_APP_SERVER,
+            url: '/people'
+        }
+        console.log(this.state.displayedFacts)
+        const factPersonResponse = await axios(config)
+        this.setState({ displayedFacts: factPersonResponse.data.Results })
+        console.log('This is a person', this.state.displayedFacts);
+    }
 
-  render() {
-    return (
-      <>
-      <Form>
-        <Form.Label>Category Selection</Form.Label>
-        <Form.Select onChange={this.handleTagChange} >
-          <option value="none">Make a Selection</option>
-        {this.state.tagList.map((tag, idx) => (
-          
-            <option value={tag.name}>{this.state.tagList[idx].name}</option>
-          
-        ))}
-        </Form.Select>
-      </Form>
-      <Form>
-        <Form.Label>Person Selection</Form.Label>
-        <Form.Select onChange={this.handlePeopleChange}>
-        <option value="none">Make a Selection</option>
-        {this.state.peopleList.map((person, idx) => (
-          
-            <option value={person.name}>{this.state.peopleList[idx].name}</option>
-          
-        ))}
-        </Form.Select>
-      </Form>
-      
-      {this.state.factByPerson.map((person, idx) => (
-          
-         
-          
-          <Card style={{ width: '18rem' }} key={idx}>
-              <Card.Body>
-                <Card.Title>
-                <h3>{person.people[0]}</h3>
-                </Card.Title>
-                <Card.Text>
-                {person.text}
-                </Card.Text>
-              </Card.Body>
-              <Button onClick={this.personFactFavorite}>Add to Favorites!</Button>
-              </Card>
-     ))}
+    // createFact = async (factPeople, factText, factSource) => {
+    //   if (this.props.auth0.isAuthenticated) {
+    //     const res = await this.props.auth0.getIdTokenClaims();
+    //     const jwt = res.__raw;
+    //     console.log("jwt: ", jwt);
+    //     let newFact = {
+    //       people: factPeople,
+    //       text: factText,
+    //       source: factSource,
+    //     }
+    //     console.log('New fact being created (location factspage.js) FROM FORM unless otherwise specified', newFact);
+    //     const config = {
+    //       headers: { "Authorization": `Bearer ${jwt}` },
+    //       params: { email: this.props.auth0.user.email },
+    //       method: 'post',
+    //       baseURL: process.env.REACT_APP_SERVER,
+    //       url: `/userfacts`,
+    //       data: newFact
+    //     }
+    //     const createResponse = await axios(config);
+    //     console.log(createResponse.data);
+    //     let newFavArr = this.state.userFavArr;
+    //     newFavArr.push(newFact);
+    //     this.setState({ userFavArr: newFavArr })
+    //   } else {
+    //     console.error("Invalid authentification.")
+    //   }
+    // }
 
-{this.state.factByTag.map((tagName, idx) => (
-          
-         
-          
-          <Card style={{ width: '18rem' }} key={idx}>
-              <Card.Body>
-                <Card.Title>
-                <h3>{this.state.selectedTag}</h3>
-                </Card.Title>
-                <Card.Text>
-                {tagName.text}
-                </Card.Text>
-              </Card.Body>
-              <Button>Add to Favorites!</Button>
-              </Card>
-     ))}
+    // personFactFavorite = async (fact) => {
+    //   console.log('New fact being created by FAVORITING', fact);
+    //   await this.createFact(fact.people, fact.text, fact.source);
+    // }
 
-      </>
-      
-    );
-  }
+    render() {
+        return (
+            <>
+                <Form>
+                    <Form.Label>Category Selection</Form.Label>
+                    <Form.Select onChange={this.handleTagChange} >
+                        <option value="none">Make a Selection</option>
+                        {this.state.tagList.map((tag, idx) => (
+
+                            <option value={tag.name}>{this.state.tagList[idx].name}</option>
+
+                        ))}
+                    </Form.Select>
+                </Form>
+                <Form>
+                    <Form.Label>Person Selection</Form.Label>
+                    <Form.Select onChange={this.handlePeopleChange}>
+                        <option value="none">Make a Selection</option>
+                        {this.state.peopleList.map((person, idx) => (
+
+                            <option value={person.name}>{this.state.peopleList[idx].name}</option>
+
+                        ))}
+                    </Form.Select>
+                </Form>
+
+                {this.state.displayedFacts.map((fact, idx) => (
+
+                    <Card style={{ width: '18rem' }} key={idx}>
+                        <Card.Body>
+                            <Card.Title>
+                                <h3>{fact.people ? fact.people[0] : this.state.selectedTag}</h3>
+                            </Card.Title>
+                            <Card.Text>
+                                {fact.text}
+                            </Card.Text>
+                        </Card.Body>
+                        <Button onClick={() => this.props.createFactFromFavorite(fact)}>Add to Favorites!</Button>
+                    </Card>
+                ))}
+
+                {
+                //this.state.displayedFacts.map((tagName, idx) => (
+
+
+
+                //     <Card style={{ width: '18rem' }} key={idx}>
+                //         <Card.Body>
+                //             <Card.Title>
+                //                 <h3>{this.state.selectedTag}</h3>
+                //             </Card.Title>
+                //             <Card.Text>
+                //                 {tagName.text}
+                //             </Card.Text>
+                //         </Card.Body>
+                //         <Button>Add to Favorites!</Button>
+                //     </Card>
+                // ))
+                }
+
+            </>
+
+        );
+    }
 };
 
 export default withAuth0(FactsPage);
